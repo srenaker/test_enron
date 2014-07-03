6268a267aa926c46f475e16a348d6bb0969f6ec6
Steve Renaker <srenaker@legalzoom.com>
2014-07-03 11:33:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnronTest::Application.config.secret_key_base = '4c85e08aa63fdc1f448b8384af7ad88f6c46f814ea9503d228f68a7966ff4b6d2f891872843aaf447ede444c97bdb22deb4d9c2a8baa21ef8634fcd4b18ebbb4'
