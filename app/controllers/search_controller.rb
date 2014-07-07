9810f807132c9bce3a9f9cb160f36ab771048f0f
Steve Renaker <srenaker@legalzoom.com>
2014-07-07 15:08:27 -0700

    checking in for the day

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,24 @@
 class SearchController < ApplicationController
   
   def search_results
-    type = params[:type]
+    @type = params[:type]
     
-    if type == 'from'
+    if @type == 'from'
       @from_address = params[:from_address]
-      messages = Message.where('headers.From' => @from_address).all
-
-      @results = Kaminari.paginate_array(messages).page(params[:page]).per(10)
-    elsif type == 'keywords'
+      messages = MongoMapper.database['messages'].find({'headers.From' => @from_address}).to_a
+      
+    elsif @type == 'keywords'
       @keywords = params[:keywords]
-      @results = MongoMapper.database['messages'].find({"$text" => {"$search" => @keywords}}).limit(100).to_a
+      messages = MongoMapper.database['messages'].find({"$text" => {"$search" => @keywords}}).limit(100).to_a
+      
     end
     
+    @num_results = messages.length
+    
+    messages.each do |m|
+      unless 
+    
+    @results = Kaminari.paginate_array(messages).page(params[:page]).per(10)
   end
 
 end
